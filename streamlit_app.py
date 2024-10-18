85a2ac3908ed2e2465f3ef943743abef1ac53b19
DRetlaw <maildwal@gmail.com>
2024-10-18 12:59:06 +0000

    exception handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,12 +49,16 @@ else:
         # configure Gemini Model
         model = genai.GenerativeModel('gemini-1.0-pro-latest')
         int_res = model.generate_content(prompt,stream=True)
-        res = int_res.resolve()
-        stream = res.text
+        try:
+            res = int_res.resolve()
+            if res:
+                stream = res.text
 
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "user", "parts": response})
+                # Stream the response to the chat using `st.write_stream`, then store it in session state
+                with st.chat_message("user"):
+                    response = st.write_stream(stream)
+                st.session_state.messages.append({"role": "user", "parts": response})
+            else:
+                st.write("Error generating response.")
+        except Exception as e:
+            st.write(f"An error occurred: {e}")
